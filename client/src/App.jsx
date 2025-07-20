b1f47703d04256ac846a8f973fd41cf9f1f0c2c0
Limon00001 <limonhossain82@gmail.com>
2025-07-20 12:26:24 +0600

    feat(client-project): initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 20 Jul, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// App Component
+const App = () => {
+  return <div className="text-3xl">App</div>;
+};
+
+// Export
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 20 Jul, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Internal Imports
+import App from './App.jsx';
+
+// Styles
+import './index.css';
+
+// Render App
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
