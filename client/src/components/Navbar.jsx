2a2de06d37204b1f6c47d1c1a495d9a6493b6776
Limon00001 <limonhossain82@gmail.com>
2025-07-20 15:12:31 +0600

    feat(client-auth): integrate Clerk for user authentication

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,6 +6,7 @@
  */
 
 // External Imports
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 import { ArrowRight } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
@@ -15,17 +16,26 @@ import Logo from './Logo';
 // Navbar Component
 const Navbar = () => {
   const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
 
   return (
     <div className="fixed w-full z-5 backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
       {/* Logo */}
       <Logo onClick={() => navigate('/')} />
 
-      {/* Button Group */}
-      <button className="group flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">
-        Get Started{' '}
-        <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-all group-hover:duration-300" />
-      </button>
+      {user ? (
+        <UserButton />
+      ) : (
+        // Button Group
+        <button
+          onClick={openSignIn}
+          className="group flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        >
+          Get Started{' '}
+          <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-all group-hover:duration-300" />
+        </button>
+      )}
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@
  */
 
 // External Imports
+import { ClerkProvider } from '@clerk/clerk-react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -16,11 +17,20 @@ import App from './App.jsx';
 // Styles
 import './index.css';
 
+// Import Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
 // Render App
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 );
