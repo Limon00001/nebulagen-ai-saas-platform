dd53bec19e2231cd833146c333bd4ca482bbb948
Limon00001 <limonhossain82@gmail.com>
2025-07-21 19:32:42 +0600

    feat(server-project): setup the initial server

---
+++ server/server.js
@@ -0,0 +1,30 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 21 Jul, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+
+// Create Express App
+const app = express();
+
+// Port
+const port = process.env.PORT || 5001;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Server is running...');
+});
+
+// Start Server
+app.listen(port, () => console.log(`Server is running on port ${port}`));
