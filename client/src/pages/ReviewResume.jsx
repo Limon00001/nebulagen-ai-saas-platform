7d6d4a7f3ba6c449761c533c52f346b4be4dc579
Limon00001 <limonhossain82@gmail.com>
2025-07-22 22:37:39 +0600

    feat(client-resume-analysis): implemented resume analysis generation

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -6,20 +6,56 @@
  */
 
 // External Imports
+import { useAuth } from '@clerk/clerk-react';
+import axios from 'axios';
 import { FileText, Sparkles } from 'lucide-react';
 import { useState } from 'react';
+import { toast } from 'react-hot-toast';
+import Markdown from 'react-markdown';
+
+// Base URL
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 // Review Resume
 const ReviewResume = () => {
   const [inputData, setInputData] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState('');
+
+  // Get User Token
+  const { getToken } = useAuth();
 
   // Form Submit Handler
   const handleSubmit = async (e) => {
+    e.preventDefault();
+
     try {
-      e.preventDefault();
+      // Enable Loading Indicator
+      setLoading(true);
+
+      // Form Data for image
+      const formData = new FormData();
+      formData.append('resume', inputData);
+
+      // Api Call
+      const { data } = await axios.post('/api/ai/resume-review', formData, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      });
+
+      // If api call is successful
+      if (data?.success) {
+        setContent(data?.content);
+      } else {
+        toast.error(data?.message);
+      }
     } catch (error) {
       console.error(error);
     }
+
+    // Disable Loading Indicator
+    setLoading(false);
   };
 
   return (
@@ -51,9 +87,23 @@ const ReviewResume = () => {
           Only PDF files are supported
         </p>
 
-        <button className="w-full flex items-center justify-center gap-2 bg-gradient-to-r from-emerald-600 to-emerald-600/90 text-white px-4 py-2 rounded-lg text-sm mt-6 transition cursor-pointer">
-          <FileText className="w-5 text-white" />
-          Review Resume
+        <button
+          disabled={loading}
+          className={`w-full flex items-center justify-center gap-2 bg-gradient-to-r from-emerald-600 to-emerald-600/90 text-white px-4 py-2 rounded-lg text-sm mt-6 transition ${
+            loading ? 'opacity-70 cursor-not-allowed' : 'cursor-pointer'
+          }`}
+        >
+          {loading ? (
+            <>
+              <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+              Analyzing Resume ...
+            </>
+          ) : (
+            <>
+              <FileText className="w-5 text-white" />
+              Review Resume
+            </>
+          )}
         </button>
       </form>
 
@@ -64,12 +114,20 @@ const ReviewResume = () => {
           <h1 className="text-xl font-semibold">Analysis Results</h1>
         </div>
 
-        <div className="flex-1 flex justify-center items-center">
-          <div className="flex flex-col items-center gap-5 text-sm text-gray-400">
-            <FileText className="w-9 h-9" />
-            <p>Upload a resume and click "Review Resume" to get started</p>
+        {!content ? (
+          <div className="flex-1 flex justify-center items-center">
+            <div className="flex flex-col items-center gap-5 text-sm text-gray-400">
+              <FileText className="w-9 h-9" />
+              <p>Upload a resume and click "Review Resume" to get started</p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw">
+              <Markdown>{content}</Markdown>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
